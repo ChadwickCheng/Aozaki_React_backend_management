a72fdbc7d7bb7c056681f787c7ddb0adaa93a796
Dan Cheng <ChadwickCheng@outlook.com>
2024-03-30 17:52:06 +0800

    图表初始化

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,38 @@
+import * as echarts from 'echarts';
+import { useEffect, useRef } from 'react';
+
 const Home = () => {
+  const chartRef = useRef(null);
+  useEffect(() => {
+    // 保证dom可用后再进行渲染 且对应div必须有宽高
+    // 获取渲染图表的dom节点
+    const chartDom = chartRef.current;
+    // 初始化图表生成实例
+    const myChart = echarts.init(chartDom);
+    // 准备图表参数
+    const option = {
+      xAxis: {
+        type: 'category',
+        data: ['vue', 'react', 'angular']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [10,40,70],
+          type: 'bar'
+        }
+      ]
+    };
+    // 使用图表参数完成渲染
+    option && myChart.setOption(option);
+  }, []);
   return (
     <div>
-      <h1>Home</h1>
+      <div ref={chartRef} style={{width:'500px',height:'400px'}}>
+
+      </div>
     </div>
   );
 }
