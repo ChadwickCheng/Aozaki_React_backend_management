29886d4793f2b031b681c34fdada8bc41f0fd700
Dan Cheng <ChadwickCheng@outlook.com>
2024-03-31 15:20:03 +0800

    路由懒加载

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,9 +2,15 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../pages/Layout/index.jsx";
 import Login from "../pages/Login/index.jsx";
 import { AuthRoute } from '@/components/AuthRoute.jsx'
-import Home from "@/pages/Home/index.jsx";
-import Article from "@/pages/Article/index.jsx";
-import Publish from "@/pages/Publish/index.jsx";
+// import Home from "@/pages/Home/index.jsx";
+// import Article from "@/pages/Article/index.jsx";
+// import Publish from "@/pages/Publish/index.jsx";
+import { Suspense, lazy } from "react";
+
+// 1. lazy导入
+const Home = lazy(()=>import('@/pages/Home/index.jsx'));
+const Article = lazy(()=>import('@/pages/Article/index.jsx'));
+const Publish = lazy(()=>import('@/pages/Publish/index.jsx'));
 
 const router = createBrowserRouter([
   {
@@ -13,15 +19,15 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Home/>
+        element: <Suspense fallback="loading"><Home/></Suspense>
       },
       {
         path: 'article',
-        element: <Article/>
+        element: <Suspense fallback="loading"><Article/></Suspense>
       },
       {
         path: 'publish',
-        element: <Publish/>
+        element: <Suspense fallback="loading"><Publish/></Suspense>
       },
     ]
   },
